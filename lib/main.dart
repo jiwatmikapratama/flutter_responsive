e0db509f8cf330e14f5f379c79a533dde562f235
jiwatmikapratama <jiwatmikapratama@gmail.com>
2022-10-15 20:58:42 +0800

    Membuat breakpoints

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive/screens/homepage.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, child!),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(350, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(600, name: TABLET),
+          const ResponsiveBreakpoint.resize(800, name: DESKTOP),
+          const ResponsiveBreakpoint.autoScale(1700, name: "XL"),
+        ],
+      ),
       debugShowCheckedModeBanner: false,
       home: Homepage(),
     );
